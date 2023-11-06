bbf4082947a03e7054ae375f18beab864c4a6b56
Kyosuke Fujimoto <kyoro.f@gmail.com>
2023-11-06 20:11:35 +0900

    Define error variables

--- asemo/send_email.go
+++ asemo/send_email.go
@@ -15,14 +15,22 @@ func (s *Server) sendEmail(c echo.Context) error {
 	var req SendEmailRequest
 	err := c.Bind(&req)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "bad request") // fixme
+		return sendEmailErrorResponse(c, BadRequestException)
 	}
 	resp, e := s.sendEmailHandler(&req)
 	if e != nil {
-		c.Response().Header().Set("x-amzn-errortype", e.ErrorName)
-		return c.JSON(e.StatusCode, e.SesErrorResponse)
+		return sendEmailErrorResponse(c, e)
 	}
+	return sendEmailSuccessResponse(c, resp)
+}
+
+func sendEmailSuccessResponse(c echo.Context, resp *SendEmailResponse) error {
 	return c.JSON(http.StatusOK, resp)
+}
+
+func sendEmailErrorResponse(c echo.Context, e *SendEmailError) error {
+	c.Response().Header().Set("x-amzn-errortype", e.ErrorName)
+	return c.JSON(e.StatusCode, e.SesErrorResponse)
 }
 
 type SendEmailRequest struct {
@@ -97,3 +105,106 @@ type SendEmailError struct {
 	ErrorName  string
 	SesErrorResponse
 }
+
+var (
+	// common errors
+	// https://docs.aws.amazon.com/ses/latest/APIReference-V2/CommonErrors.html
+	AccessDeniedException = &SendEmailError{
+		StatusCode: 403,
+		ErrorName:  "AccessDeniedException",
+	}
+	ExpiredTokenException = &SendEmailError{
+		StatusCode: 403,
+		ErrorName:  "ExpiredTokenException",
+	}
+	IncompleteSignature = &SendEmailError{
+		StatusCode: 403,
+		ErrorName:  "IncompleteSignature",
+	}
+	InternalFailure = &SendEmailError{
+		StatusCode: 500,
+		ErrorName:  "InternalFailure",
+	}
+	MalformedHttpRequestException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "MalformedHttpRequestException",
+	}
+	NotAuthorized = &SendEmailError{
+		StatusCode: 401,
+		ErrorName:  "NotAuthorized",
+	}
+	OptInRequired = &SendEmailError{
+		StatusCode: 403,
+		ErrorName:  "OptInRequired",
+	}
+	RequestAbortedException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "RequestAbortedException",
+	}
+	RequestEntityTooLargeException = &SendEmailError{
+		StatusCode: 413,
+		ErrorName:  "RequestEntityTooLargeException",
+	}
+	RequestExpired = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "RequestExpired",
+	}
+	RequestTimeoutException = &SendEmailError{
+		StatusCode: 408,
+		ErrorName:  "RequestTimeoutException",
+	}
+	ServiceUnavailable = &SendEmailError{
+		StatusCode: 503,
+		ErrorName:  "ServiceUnavailable",
+	}
+	ThrottlingException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "ThrottlingException",
+	}
+	UnrecognizedClientException = &SendEmailError{
+		StatusCode: 403,
+		ErrorName:  "UnrecognizedClientException",
+	}
+	UnknownOperationException = &SendEmailError{
+		StatusCode: 404,
+		ErrorName:  "UnknownOperationException",
+	}
+	ValidationError = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "ValidationError",
+	}
+
+	// special errors
+	AccountSuspendedException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "AccountSuspendedException",
+	}
+	BadRequestException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "BadRequestException",
+	}
+	LimitExceededException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "LimitExceededException",
+	}
+	MailFromDomainNotVerifiedException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "MailFromDomainNotVerifiedException",
+	}
+	MessageRejected = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "MessageRejected",
+	}
+	NotFoundException = &SendEmailError{
+		StatusCode: 404,
+		ErrorName:  "NotFoundException",
+	}
+	SendingPausedException = &SendEmailError{
+		StatusCode: 400,
+		ErrorName:  "SendingPausedException",
+	}
+	TooManyRequestsException = &SendEmailError{
+		StatusCode: 429,
+		ErrorName:  "TooManyRequestsException",
+	}
+)
